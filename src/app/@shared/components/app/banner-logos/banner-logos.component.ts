21a46b890eac7b838cd30e3c391614dfff61c0b2
Sara Mohamed <66149235+sara7878@users.noreply.github.com>
2022-11-16 16:58:19 +0200

    updates in carosel of logos and banners

--- src/app/@shared/components/app/banner-logos/banner-logos.component.ts
+++ src/app/@shared/components/app/banner-logos/banner-logos.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Banner, Logo } from '@app/@core/interfaces/_app/app-response';
-import { BannerConfig, logoConfig } from '@app/@core/interfaces/_app/banner-logo-config';
+import { BannerConfig, logoConfig, logoConfig1 } from '@app/@core/interfaces/_app/banner-logo-config';
 import { BannersLogoservice } from '@app/@core/services/Banners-logos.service';
 import { Router } from '@angular/router';
 
@@ -13,26 +13,38 @@ export class BannerLogosComponent implements OnInit {
  x=0;
   @Input() loading:boolean = true 
   @Input() type:string = '' 
+  @Input() num:string = '' 
 
   BannerConfig = BannerConfig 
   LogoConfig = logoConfig 
+  LogoConfig1 = logoConfig1 
+  myClonedArray!:Logo[]
   constructor(
     private logoBannerService: BannersLogoservice,
     private route: Router) { }
 
   ngOnInit(): void {
     this.BannerConfig.banner=[]
-    this.logoBannerService.getBanner().subscribe( res => {
+    this.LogoConfig.banner=[]
+    this.LogoConfig1.banner=[]
+
+      this.logoBannerService.getBanner().subscribe( res => {
      this.BannerConfig.banner =res as Banner[]
-     
     })
 
-    this.logoBannerService.getLogo().subscribe( res => {
-      this.LogoConfig.banner=[]
 
-      this.LogoConfig.banner =res as Logo[]
+    if(this.type == 'logo'){
+          this.logoBannerService.getLogo().subscribe( res => {
+          this.LogoConfig.banner =res as Logo[]
+          if(this.num == '2'){    
+            this.LogoConfig1.banner = res?.slice().reverse() || [];
+          }
+
      })
+    }
 
+
+  
 
   }
   onClick(id:number) {
